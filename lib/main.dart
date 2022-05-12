aed40554829d6b19248800337492a766484ebf10
raminux <ramin.esmzad@hotmail.com>
2022-05-12 21:28:06 +0430

    Working with Scaffold and its props

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text("I am Rich."),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am Rich', ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image.network('https://www.cameraegg.org/wp-content/uploads/2015/06/canon-powershot-g3-x-sample-images-1.jpg',),
+      ),
     ),
   ));
 }
